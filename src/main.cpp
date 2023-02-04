b68a0cabca6fce94eea54fee97a4f6c429325e18
orion <oletizi@mac.com>
2023-02-04 15:26:39 -0800

    * Added support for Adafruit_SSD1306 OLED 128x64 display * Added Inconsolata font headers
    On branch main
    Changes to be committed:
    	new file:   include/Fonts/Inconsolata12ptb.h
    	new file:   include/Fonts/Inconsolata9pt7b.h
    	modified:   platformio.ini
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/Inconsolata9pt7b.h>
 
-void setup() {
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define LINE_HEIGHT 12
+// #define OLED_RESET     4
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.setFont(&Inconsolata_Regular8pt7b);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, LINE_HEIGHT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+unsigned long checkpoint = 0;
+unsigned long now = 0;
+
+void loop()
+{
+  now = millis();
+  if (now - checkpoint >= 1000)
+  {
+    checkpoint = now;
+    Serial.printf("Now: %lu\n", now);
+    display.clearDisplay();
+    display.setCursor(0, LINE_HEIGHT);
+    display.printf("Now: %lu\n", now);
+    display.display();
+  }
 }
+
