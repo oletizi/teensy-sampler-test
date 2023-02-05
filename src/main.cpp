65ab92fc07117bacb4a2dc21a5fc728cce015a65
orion <oletizi@mac.com>
2023-02-04 16:04:20 -0800

    cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,12 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define LINE_HEIGHT 12
-// #define OLED_RESET     4
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
   {
@@ -45,4 +43,3 @@ void loop()
     display.display();
   }
 }
-
